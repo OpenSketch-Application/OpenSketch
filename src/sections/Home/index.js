d81d8c5d22902b9ca8fbb7e669c747efdb953b11
ragu-S <ragu.sivanandha@gmail.com>
2015-05-30 22:51:11 -0400

    adding whiteboard form submission and removed duplicate socket call

--- src/sections/Home/index.js
+++ src/sections/Home/index.js
@@ -5,8 +5,11 @@ var find = require('dom-select');
 var states = require('./states');
 var f1 = require('f1');
 
+var Whiteboard;
 
 module.exports = Section;
+// module.exports = Whiteboard;
+exports = Whiteboard;
 
 function Section() {}
 
@@ -17,13 +20,21 @@ Section.prototype = {
     var hbs = fs.readFileSync(__dirname + '/index.hbs', 'utf8');
     var infobox = find('#info-box');
 
-    //states.init.textbox.position[1] = infobox.offsetWidth;
-
-    // this.animate.go = new f1({
-
-    // });
 
     content.innerHTML = hbs;
+
+    // whiteboard options Section for user to create a whiteboard
+    find('div.control:last-child button').addEventListener('click', function(e) {
+      e.preventDefault();
+
+      console.log('creating whiteboard');
+      Whiteboard = getWhiteboardSettings();
+    });
+    // Get all input elements
+
+    //var whiteboardSettings =
+
+
     done();
   },
 
@@ -43,3 +54,15 @@ Section.prototype = {
     done();
   }
 };
+
+
+function getWhiteboardSettings() {
+  var inputElements = document.querySelectorAll('form input');
+
+  return Array.prototype.slice
+          .apply(inputElements)
+            .reduce(function(settings, input) {
+              settings[input.name] = input.value;
+              return settings;
+            }, {});
+}
