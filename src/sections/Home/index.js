248b30f08390ffde95f7c482152e7b87780e2b32
Benson Wong <wongbsn@gmail.com>
2015-05-31 16:45:26 -0400

    Created whiteboard view

--- src/sections/Home/index.js
+++ src/sections/Home/index.js
@@ -1,8 +1,8 @@
 var fs = require('fs');
+var f1 = require('f1');
+var find = require('dom-select');
 var framework = require('../../framework/index');
 var model = require('../../model/model');
-var find = require('dom-select');
-var f1 = require('f1');
 var states = require('./states');
 
 module.exports = Section;
@@ -13,17 +13,21 @@ Section.prototype = {
 
   init: function(req, done) {
     var content = find('#content');
-    content.innerHTML = fs.readFileSync(__dirname + '/index.hbs', 'utf8');
+    var section = document.createElement('div');
+    section.innerHTML = fs.readFileSync(__dirname + '/index.hbs', 'utf8');
+    content.appendChild(section);
+
+    states.out.home.position[0] = -document.body.offsetWidth;
+
     this.animate = new f1().states(states)
                            .transitions(require('./transitions'))
                            .targets({ textbox1: find('#textbox1'), 
                                       textbox2: find('#textbox2'), 
-                                      textbox3: find('#textbox3')
+                                      textbox3: find('#textbox3'),
+                                      home: find('#home')
                                     })
                            .parsers(require('f1-dom'))
                            .init('init');
-
-    this.animate.go('idle');
 
     // whiteboard options Section for user to create a whiteboard
     find('div.control:last-child button').addEventListener('click', function(e) {
@@ -31,7 +35,9 @@ Section.prototype = {
 
       console.log('creating whiteboard');
       Whiteboard = getWhiteboardSettings();
-    });
+      this.animateOut();
+      framework.go('/whiteboard');
+    }.bind(this));
 
     done();
   },
@@ -40,14 +46,18 @@ Section.prototype = {
   },
 
   animateIn: function(req, done) {
-    // this.animate.go('idle', function() {
-    //   done();
-    // });
+    this.animate.go('idle', function() {
+      if(done)
+        done();
+    }.bind(this));
   },
 
   animateOut: function(req, done) {
-    done();
-
+    console.log('animate out');
+    this.animate.go('out', function() {
+      if(done)
+        done();
+    }.bind(this));
   },
 
   destroy: function(req, done) {
--- src/sections/Home/states.js
+++ src/sections/Home/states.js
@@ -11,6 +11,10 @@ module.exports = {
         textbox3: {
             alpha: -2,
             position: [ -30, 105, 0 ]
+        },
+        home: {
+            alpha: 1,
+            position: [ 0, 0, 0 ]
         }
     },
     idle: {
@@ -25,39 +29,28 @@ module.exports = {
         textbox3: {
             alpha: 1,
             position: [ 0, 105, 0 ]
+        },
+        home: {
+            alpha: 1,
+            position: [ 0, 0, 0 ]
         }
     },
     out: {
-        textbox: {
-            alpha: 0,
+        textbox1: {
+            alpha: 1,
+            position: [ 0, 0, 0 ]
+        },
+        textbox2: {
+            alpha: 1,
+            position: [ 0, 53, 0 ]
+        },
+        textbox3: {
+            alpha: 1,
+            position: [ 0, 105, 0 ]
+        },
+        home: {
+            alpha: 1,
             position: [ 0, 0, 0 ]
         }
     }
 };
-
-// module.exports = {
-//     init: function(statename) {
-//         return {
-//             textbox: {
-//                 alpha: 0,
-//                 position: [ 0, 0, 0 ]
-//             }
-//         };
-//     },
-//     idle: function(statename) {
-//         return {
-//             textbox: {
-//                 alpha: 1,
-//                 position: [ 0, 50, 0 ]
-//             }
-//         };
-//     },
-//     out: function(statename) {
-//         return {
-//             textbox: {
-//                 alpha: 0,
-//                 position: [ 0, 0, 0 ]
-//             }
-//         };
-//     }
-// };
